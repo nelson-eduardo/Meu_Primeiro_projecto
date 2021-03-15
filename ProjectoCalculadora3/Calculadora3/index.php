5e801fa2cc14396ac8a294cb9e97f75646d73936
nelsonEduardo <nelsoneduardo@sapo.ao>
2021-03-15 17:21:08 +0100

    Enviar os arquivos

---
+++ ProjectoCalculadora3/Calculadora3/index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>ja fiz varios textes</title>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="./css/style.css">
+    
+  </head>
+  <body>
+    
+    <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top py-3 box-shadow">
+      <a href="index.html" class="navbar-brand d-none d-md-block">
+        <img src="img/caravan.svg" alt="Caravan">
+      </a>
+      
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Abrir Navegação">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link " href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Soma</a>
+            
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="planos.html">Subtracao</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contato.html">Mutiplicacao</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contato.html">Divisao</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="btn btn-outline-primary ml-md-2" href="inscricao.html">Inscreva-se</a>
+          </li>
+        </ul>
+      </div>
+       
+    </nav>
+     <!--Menu navegacao -->
+
+      <section class="container-fluid introducao ">
+          
+
+          <div class="row bg-dark text-white">
+            <div class="col-lg-7 p-0 ">
+              <img src="img/dublin.jpg" id="IntroEsquerdo">
+            </div>
+            <div class="col-lg-5">
+              <h1>Maquina calculadora</h1>
+
+              <p> calculador criada como exemplo pessoal. Estou trabalhando com githubgit<br>Veja o resultdao embaixo</p>
+              
+            </div>
+          </div>
+      </section>
+        <!--Fim bloco introducao e carroseel -->
+      
+      <section class="py-3 bg-light text-center  ">
+        <div class=" my-5">
+          <h1 class=" display-4 text-primary">Exprimenta</h1>
+          <span class="d-block h6 pullquote-left">A melhor calculadora de sempre</span>
+        </div>
+       
+       <!--Primeira linha d desta secao-->
+        <div class="container ">
+          
+          <div class="row">
+
+            <div class="col-xl-4 col-md-6">
+              <div class="d-flex justify-content-center"style="height: 100px">
+              <img src="img/icones/praias.svg">
+              </div>
+              <h3>calculos de soma</h3>
+               <div class="divider-short"></div>
+              <p class="pullquote-left">Calculo de soma de maneiras mais facil, nunca foi mais facil fazer soma numeros</p>
+            </div>
+
+            <div class="col-xl-4 col-md-6">
+               <div class="d-flex justify-content-center"style="height: 100px">
+                 <img src="img/icones/translado.svg">
+               </div>
+              <h3>calculos de soma</h3>
+              <p>Calculo de soma de maneiras mais facil, nunca foi mais facil fazer soma numeros</p>
+            </div>
+
+            <div class="col-xl-4 col-md-6">
+               <div class="d-flex justify-content-center" style="height: 100px">
+                 <img src="img/icones/turismo.svg">
+               </div>
+              <h3>calculos de soma</h3>
+              <p>Calculo de soma de maneiras mais facil, nunca foi mais facil fazer soma numeros</p>
+            </div>
+          </div>
+
+
+           <div class="row py-3">
+            <div class="col-xl-4 col-md-6">
+               <div class="d-flex justify-content-center" style="height: 100px">
+              <img src="img/icones/passagens.svg">
+               </div>
+              <h3>calculos de soma</h3>
+              <p>Calculo de soma de maneiras mais facil, nunca foi mais facil fazer soma numeros</p>
+            </div>
+
+            <div class="col-xl-4 col-md-6">
+               <div class="d-flex justify-content-center" style="height: 100px">
+              <img src="img/icones/bagagens.svg">
+               </div>
+              <h3>calculos de soma</h3>
+              <p>Calculo de soma de maneiras mais facil, nunca foi mais facil fazer soma numeros</p>
+            </div>
+
+            <div class="col-xl-4 col-md-6">
+               <div class="d-flex justify-content-center" style="height: 100px">
+              <img src="img/icones/passaporte.svg">
+               </div>
+              <h3>calculos de soma</h3>
+              <p>Calculo de soma de maneiras mais facil, nunca foi mais facil fazer soma numeros</p>
+            </div>
+            
+          </div>
+
+          <a href="comprar.html" class="btn btn-primary btn-lg mt-4" > Compra Planos</a>
+          
+        </div>
+        
+      </section>
+      <!--Fim bloco servicoa e planos -->
+    
+
+
+      <!--Inicio seccao Quote -->
+      <section id="home-Quote" class="p-md-5">
+              <blockquote  class="blockquote text-center text-white p-md-5 p-sm-3"> 
+                <p class="display-4">Afelecidade nao esta na estrada que leva a um lugar. A felecidade e a propria estrada </p> 
+                <footer class="blockquote-footer text-white">Bob Dylan</footer>
+              </blockquote>
+      </section>
+
+      <!--Fim Seccao quote -->
+
+    
+      <!--Inicio seccao perguntas frquentes -->
+      <section class="py-3 bg-light ">
+        <div class=" my-5 text-center  ">
+          <h1 class=" display-4 text-primary">Estas com duvidas</h1>
+          <span class="d-block h6 pullquote-left">Perguntas Frequentes</span>
+          
+        </div>
+      </section>
+
+    
+    <script type="text/javascript" src="js/jquery-3.2.1.slim.min.js"></script>
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="js/app.js"></script>
+  </body>
+</html>
